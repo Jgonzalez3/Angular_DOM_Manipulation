4cc9487ce926d75dd6d4a70b90004aa650f8836f
Javier Gonzalez <javig56@gmail.com>
2018-05-10 11:30:01 -0700

    added event keyup for search of task

--- RestfulTaskAPI/RestfulAPI/src/app/app.component.ts
+++ RestfulTaskAPI/RestfulAPI/src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent implements OnInit{
   title = 'Restful Tasks API';
   tasks: string[];
   task: string;
+  eventword: string;
   constructor(private _httpService: HttpService) {
   }
   // ngOnInit is like document ready
@@ -17,8 +18,10 @@ export class AppComponent implements OnInit{
     // this.getTasksFromService()
     this.tasks
     this.task
+    this.eventword = ""
   }
   getTasksFromService(){
+
     let observable = this._httpService.getTasks();
     observable.subscribe(data => {
       console.log("Got our TASKS", data)
@@ -30,6 +33,28 @@ export class AppComponent implements OnInit{
     console.log(id);
     for(var x = 0; x<this.tasks.length; x++){
       if(id == this.tasks[x]._id){
+        this.task = this.tasks[x];
+        console.log(this.task);
+      }
+    }
+  }
+  getTaskFromKeyup(event: any){
+    console.log(event)
+    console.log(`EVENT, ${event.key}`);
+    //regex below searches for letters only and whitespaces:
+    var letters = /^[A-Za-z-\s]+$/;
+    var eventkey = event.key;
+    if(eventkey.match(letters) && eventkey.length < 2){
+      this.eventword += event.key;
+    }
+    console.log(event.code)
+    //conditional below is to delete last letter in string eventword with slice method
+    if(event.code == "Backspace"){
+      this.eventword = this.eventword.slice(0,this.eventword.length-1);
+    }
+    console.log(this.eventword);
+    for(var x = 0; x<this.tasks.length; x++){
+      if(this.eventword == this.tasks[x].title){
         this.task = this.tasks[x];
         console.log(this.task);
       }

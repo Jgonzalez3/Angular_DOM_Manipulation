bb38e75daafb9d7e53fb79e60993224a4ef67a70
Javier Gonzalez <javig56@gmail.com>
2018-05-12 17:32:58 -0700

    server clean up

--- RestfulTaskCRUD/server.js
+++ RestfulTaskCRUD/server.js
@@ -1,7 +1,6 @@
 var express = require("express");
 // path module -- try to figure out where and why we use this
 var path = require("path");
-var session = require('express-session');
 // create the express app
 var app = express();
 var bodyParser = require('body-parser');
@@ -9,18 +8,9 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 // static content
 app.use(express.static(path.join(__dirname, "./RestfulAPI/dist/RestfulAPI")));
-app.use(session({
-    secret: 'codingdojorocks',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {maxAge: 60000}
-}));
 // setting up ejs and our views folder
 app.set('views', path.join(__dirname, './views'));
-app.set('view engine', 'ejs');
 // use validation errors below but after session used
-const flash = require('express-flash');
-app.use(flash());
 
 require("./server/config/routes")(app);
 app.listen(8000, () => {
